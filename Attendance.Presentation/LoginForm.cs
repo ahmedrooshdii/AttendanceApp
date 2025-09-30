2b3a37b273fb02d3642ff4bbd4f32ec51683331c
ahmedrooshdii <ahmedrooshdii@gmail.com>
2025-10-01 00:22:35 +0300

    Building LoginForm UI with GunaUI.

---
+++ Attendance.Presentation/LoginForm.cs
@@ -0,0 +1,79 @@
+﻿using Guna.UI2.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Attendance.Presentation
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+            SetPlaceholder(tbUserName, "Username");
+            SetPlaceholder(tbPassword, "Password");
+            tbUserName.Focus();
+        }
+
+        private void Exit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void SetPlaceholder(Guna2TextBox txt, string placeholder)
+        {
+            Color originalColor = txt.ForeColor;
+            bool originalUseSystemPasswordChar = txt.UseSystemPasswordChar;
+            char originalPasswordChar = txt.PasswordChar;
+            bool isPlaceholderActive = false;
+
+            void ShowPlaceholder()
+            {
+                if (string.IsNullOrEmpty(txt.Text))
+                {
+                    isPlaceholderActive = true;
+                    txt.Text = placeholder;
+                    txt.ForeColor = Color.Gray;
+                    // disable password masking while placeholder is shown
+                    txt.UseSystemPasswordChar = false;
+                }
+            }
+
+            void HidePlaceholder()
+            {
+                if (isPlaceholderActive)
+                {
+                    isPlaceholderActive = false;
+                    txt.Text = "";
+                    txt.ForeColor = originalColor;
+                    // restore password masking
+                    txt.UseSystemPasswordChar = originalUseSystemPasswordChar;
+                    txt.PasswordChar = originalPasswordChar;
+                }
+            }
+
+            txt.GotFocus += (s, e) => HidePlaceholder();
+            txt.LostFocus += (s, e) => ShowPlaceholder();
+
+            // If user types programmatically, keep state consistent
+            txt.TextChanged += (s, e) =>
+            {
+                if (!txt.Focused && string.IsNullOrEmpty(txt.Text))
+                    ShowPlaceholder();
+                else if (!string.IsNullOrEmpty(txt.Text) && isPlaceholderActive && txt.Focused)
+                    HidePlaceholder();
+            };
+
+            // initialize
+            ShowPlaceholder();
+        }
+
+    }
+}
