10432bc4100025af8e186a30cf64e7c65f64022f
ahmedrooshdii <ahmedrooshdii@gmail.com>
2025-10-01 16:57:09 +0300

    df

---
+++ Attendance.Service/AuthService.cs
@@ -0,0 +1,41 @@
+﻿using Attendance.Domain.Contracts.Repositories;
+using Attendance.Domain.Contracts.Services;
+using Azure.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attendance.Service
+{
+    public class AuthService : IAuthService
+    {
+        private IUserRepository _repo;
+        public AuthService(IUserRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<bool> LoginAsync(string username, string password)
+        {
+            var user = await _repo.GetByUsernameAsync(username);
+            if (user == null) return false;
+
+            return VerifyPassword(password, user.PasswordHash);
+        }
+
+        private string HashPassword(string password)
+        {
+            using var sha = SHA256.Create();
+            var bytes = sha.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return Convert.ToBase64String(bytes);
+        }
+
+        private bool VerifyPassword(string password, string storedHash)
+        {
+            return HashPassword(password) == storedHash;
+        }
+    }
+}
