ad1d622710525497a33dd2b87896b02924a35c0d
azmy ahmed <azmyahmedbasuony@gmail.com>
2025-10-01 16:52:02 +0300

    admin layout

--- Attendance.Presentation/Forms/AdminDashboard.cs
+++ Attendance.Presentation/Forms/AdminDashboard.cs
@@ -1,25 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Attendance.Domain.Entities;
+using System;
 using System.Windows.Forms;
-using Attendance.Domain.Contracts.Services;
-using Attendance.Domain.Entities;
 
 namespace Attendance.Presentation.Forms
 {
     public partial class AdminDashboard : Form
     {
         private readonly User _user;
+        private readonly UserManagement _userManagementForm;
 
         public AdminDashboard(User user)
         {
             InitializeComponent();
             _user = user;
+            lblUserName.Text = $"User: {_user.UserName}";
+            lblRoleName.Text = $"Role: Admin";
+
+            // Pre-load the UserManagement form
+            _userManagementForm = new UserManagement
+            {
+                TopLevel = false,
+                FormBorderStyle = FormBorderStyle.None,
+                Dock = DockStyle.Fill
+            };
+            mainContentPanel.Controls.Add(_userManagementForm);
+
             this.FormClosed += AdminDashboard_FormClosed;
         }
 
@@ -31,5 +36,24 @@ namespace Attendance.Presentation.Forms
             }
         }
 
+        private void BtnLogout_Click(object sender, EventArgs e)
+        {
+            this.Owner.Show();
+            this.Close();
+        }
+
+        private void BtnUserManagement_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Bring the pre-loaded UserManagement form to the front
+                _userManagementForm.BringToFront();
+                _userManagementForm.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
--- Attendance.Presentation/Forms/UserManagement.cs
+++ Attendance.Presentation/Forms/UserManagement.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Attendance.Presentation.Forms
@@ -15,6 +8,30 @@ namespace Attendance.Presentation.Forms
         public UserManagement()
         {
             InitializeComponent();
+        }
+
+        private void BtnSearch_Click(object sender, EventArgs e)
+        {
+            // Implement search logic here
+            MessageBox.Show("Search functionality not implemented yet.");
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            // Implement add user logic here
+            MessageBox.Show("Add functionality not implemented yet.");
+        }
+
+        private void BtnEdit_Click(object sender, EventArgs e)
+        {
+            // Implement edit user logic here
+            MessageBox.Show("Edit functionality not implemented yet.");
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            // Implement delete user logic here
+            MessageBox.Show("Delete functionality not implemented yet.");
         }
     }
 }
